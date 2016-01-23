8197a57fd05533024667ed21d33c1d03ec77cf4c
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-01-23 21:33:15 +0100

    Add a way to detect if slug contains emojis

--- test/test.model.js
+++ test/test.model.js
@@ -2,27 +2,49 @@
 
 'use strict'
 
-require('should')
-
+const should = require('should')
 const URL = require('lib/model')
+const schema = require('lib/schema')
+
+let globalInstance
 
 describe('URL', function () {
-  it('check if "nothing" is register', function (done) {
+  it('check if a "google.com" hash is register register', function (done) {
     URL.isRegister('nothing', function (err, isRegister, results) {
       isRegister.should.be.false()
       done(err)
     })
   })
 
-  it('register "something"', function (done) {
+  it('register "google.com"', function (done) {
     URL.register('google.com', function (err, instance) {
+      globalInstance = instance
       instance.should.be.an.Object()
+
+      const instanceProperties = Object.keys(instance).length
+      const schemaProperties = Object.keys(schema).length
+
+      instanceProperties.should.be.equal(schemaProperties)
+
+      Object.keys(instance).forEach(function (key) {
+        let value = instance[key]
+        let type = schema[key].name
+        value.should.be[type]()
+      })
+
       done(err)
     })
   })
 
-  it('check if "something" is register', function (done) {
-    URL.isRegister('google.com', function (err, isRegister, result) {
+  it('check if "google.com" hash is register', function (done) {
+    URL.isRegister(globalInstance.hash, function (err, isRegister, result) {
+      isRegister.should.be.true()
+      done(err)
+    })
+  })
+
+  it('check if "google.com" emoji hash is register', function (done) {
+    URL.isRegister(globalInstance.hashEmoji, function (err, isRegister, result) {
       isRegister.should.be.true()
       done(err)
     })
