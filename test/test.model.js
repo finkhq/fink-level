8f882f903d8d0e71c59f997780d9404476607ed9
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-01-23 19:25:16 +0100

    Little adjusts

--- test/test.model.js
+++ test/test.model.js
@@ -4,25 +4,25 @@
 
 require('should')
 
-const url = require('lib/model')
+const URL = require('lib/model')
 
-describe('url', function () {
+describe('URL', function () {
   it('check if "nothing" is register', function (done) {
-    url.isRegister('nothing', function (err, isRegister, results) {
+    URL.isRegister('nothing', function (err, isRegister, results) {
       isRegister.should.be.false()
       done(err)
     })
   })
 
   it('register "something"', function (done) {
-    url.register('google.com', 'google.es', function (err, instance) {
+    URL.register('google.com', function (err, instance) {
       instance.should.be.an.Object()
       done(err)
     })
   })
 
   it('check if "something" is register', function (done) {
-    url.isRegister('google.com', function (err, isRegister, result) {
+    URL.isRegister('google.com', function (err, isRegister, result) {
       isRegister.should.be.true()
       done(err)
     })
